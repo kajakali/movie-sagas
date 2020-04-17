b77502b77bfd0e0c3b04ad5a0a22b87b5be739ae
Frieda Jacobson <kajakali@hotmail.com>
2020-04-17 16:41:49 -0500

    movies show with poster and title and description

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -5,20 +5,22 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { CardMedia } from '@material-ui/core';
 
 
 const styles = {
     card: {
       minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
+      maxWidth: 400,
     },
     title: {
       fontSize: 14,
     },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+        marginTop:30
+      },
     pos: {
       marginBottom: 12,
     },
@@ -33,7 +35,7 @@ class Listing extends Component {
 
     render() {
         console.log('listing props, ', this.props);
-      const {classes} = this.props;
+        const classes = this.props.classes;
       return (
         <div >
             <p>
@@ -41,21 +43,27 @@ class Listing extends Component {
                 it needs an onClick that goes to the details page
                 I could make it a card...
             </p>
-            <Card onClick={this.handleClick}>
+            <Card className={classes.card}
+                onClick={this.handleClick}>
+
+                    <img src={this.props.poster} alt={this.props.title}/>
+
+                <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                     {this.props.title}
                 </Typography>
-                <CardContent>
-                    <img src={this.props.poster} />
-                    <Typography component="p">
-                        {this.props.description}
-                    </Typography>
+                <Typography component="p">
+                    {this.props.description}
+                </Typography>
                 </CardContent>
             </Card>
         </div>
       );
     }
   }
+  Listing.propTypes = {
+      classes: PropTypes.object.isRequired,
+  };
   
-  export default Listing;
+  export default withStyles(styles)(Listing);
   
